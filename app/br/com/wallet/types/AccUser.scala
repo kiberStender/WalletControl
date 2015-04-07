45817d362e7837a9d73eef881555754aa9ca4b94
Kleber Stender <kleber.stender@gmail.com>
2015-04-07 07:00:54 -0300

    Added two methods: toLogin and toInsert

--- app/br/com/wallet/types/AccUser.scala
+++ app/br/com/wallet/types/AccUser.scala
@@ -1,6 +1,12 @@
 package br.com.wallet.types
 
+import play.api.libs.Codecs.sha1
+
 /**
  * Created by sirkleber on 4/5/15.
  */
-case class AccUser(accuserid: String, username: String, password: String)
+case class AccUser(accuserid: String, username: String, password: String) {
+  def toLogin = AccUser(accuserid, username, sha1(password))
+
+  def toInsert = AccUser(sha1(accuserid), username, sha1(password))
+}
