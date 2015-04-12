22269df1faf9c417d78230cd7ba698cdb001162c
Kleber Stender <kleber.stender@gmail.com>
2015-04-12 20:46:08 -0300

    Created generic methods to work asynchronally

--- app/br/com/wallet/api/controller/ActionController.scala
+++ app/br/com/wallet/api/controller/ActionController.scala
@@ -1,39 +1,41 @@
 package br.com.wallet.api.controller
 
-import br.com.wallet.api.models.result.Failure
+import br.com.wallet.api.models.result.{Success, Failure}
 import br.com.wallet.types.AccUser
-import play.api.mvc.Controller
-import play.api.mvc.Action
+import play.api.mvc._
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
-import play.api.libs.json.JsObject
-import play.api.mvc.Request
-import play.api.libs.json.JsValue
-import play.api.libs.json.Json
+import play.api.libs.json.{JsValue, JsObject, Json}
 
 /**
  * Created by sirkleber on 4/7/15.
  */
 trait ActionController extends Controller {
+  protected object NaoLogado extends Failure("Não logado!!!")
 
   protected def jsonApp = "application/json"
 
   protected def hash = "usuario"
+
+  protected type ActP[A] = (A, Request[JsValue]) => Future[Result]
+
+  protected def notLoggedIn = Future(Ok(NaoLogado.toJson) as jsonApp)
+
+  protected def post(f: (Option[String], Request[JsValue]) => Future[Result]): Action[JsValue] = Action.async(parse.json) {
+    req => f(req.session.get(hash), req)
+  }
+
+  protected def postAction(f: (AccUser, JsValue) => Future[Result]): Action[JsValue] =  post { (us, req) => us match {
+    case None => notLoggedIn
+    case Some(user) => f((Json parse user).as[AccUser], req.body)
+  }
+  }
 
   protected def getAction(f: AccUser => JsObject) = Action.async { request =>
     Future {
       Ok(request.session.get(hash) match {
         case None => Failure("Não logado") toJson
         case Some(us) => f((Json parse us).as[AccUser])
-      }) as jsonApp
-    }
-  }
-
-  protected def postAction(f: (AccUser, Request[JsValue]) => JsObject) = Action.async(parse.json) { request =>
-    Future {
-      Ok(request.session.get(hash) match {
-        case None => Failure("Não logado") toJson
-        case Some(us) => f((Json parse us).as[AccUser], request)
       }) as jsonApp
     }
   }
