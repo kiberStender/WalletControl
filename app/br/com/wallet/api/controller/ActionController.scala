a78942b239c4ef685a38b49c5c6300ecb97d8160
Kleber Stender <kleber.stender@gmail.com>
2015-04-26 21:09:24 -0300

    Removed OAuth methods

--- app/br/com/wallet/api/controller/ActionController.scala
+++ app/br/com/wallet/api/controller/ActionController.scala
@@ -21,41 +21,8 @@ trait ActionController extends Controller {
 
   protected type ActP[A] = (A, Request[JsValue]) => Future[Result]
 
-  def key = ConsumerKey("", "")
-  def google = OAuth(ServiceInfo(
-    "https://accounts.google.com/o/oauth2/token",
-    "https://your.domain.tld/authenticate/google",
-    "https://accounts.google.com/o/oauth2/auth", key), false)
-
-  protected def authenticate = Action { req =>
-    req.getQueryString("oauth_verifier").map { verifier =>
-      val tokenPair = sessionTokenPair(req).get
-
-      google.retrieveAccessToken(tokenPair, verifier) match {
-        case Right(t) => Redirect(routes.Application.index).withSession("token" -> t.token, "secret" -> t.secret)
-        case Left(e) => throw e
-      }
-    }.getOrElse(google.retrieveRequestToken("http://localhost:9000/auth") match {
-      case Right(t) => Redirect(google.redirectUrl(t.token)).withSession("token" -> t.token, "secret" -> t.secret)
-      case Left(e) => throw e
-    })
-  }
-
-  protected def sessionTokenPair(implicit request: RequestHeader): Option[RequestToken] = for {
-    token <- request.session.get("token")
-    secret <- request.session.get("secret")
-  } yield RequestToken(token, secret)
-
-  protected def notLoggedIn = Future(Ok(NaoLogado.toJson) as jsonApp)
-
   protected def post(f: (Option[String], Request[JsValue]) => Future[Result]): Action[JsValue] = Action.async(parse.json) {
     req => f(req.session.get(hash), req)
-  }
-
-  protected def postAction(f: (AccUser, JsValue) => Future[Result]): Action[JsValue] =  post { (us, req) => us match {
-    case None => notLoggedIn
-    case Some(user) => f((Json parse user).as[AccUser], req.body)
-  }
   }
 
   protected def getAction(f: AccUser => JsObject) = Action.async { request =>
