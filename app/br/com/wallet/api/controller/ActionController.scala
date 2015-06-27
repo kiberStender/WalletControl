7c02e5de04f0e7f07792cffc5668ba02bf35fda3
Kleber Stender <kleber.stender@gmail.com>
2015-06-27 13:29:26 -0300

    Created implicit timeout

--- app/br/com/wallet/api/controller/ActionController.scala
+++ app/br/com/wallet/api/controller/ActionController.scala
@@ -1,9 +1,9 @@
 package br.com.wallet.api.controller
 
+import akka.util.Timeout
 import br.com.wallet.api.models.result.{Success, Failure}
 import br.com.wallet.types.AccUser
-import controllers.routes
-import play.api.libs.oauth.{RequestToken, ServiceInfo, OAuth, ConsumerKey}
+import scala.concurrent.duration._
 import play.api.mvc._
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -13,6 +13,7 @@ import play.api.libs.json.{JsValue, JsObject, Json}
  * Created by sirkleber on 4/7/15.
  */
 trait ActionController extends Controller {
+  protected implicit lazy val timeout = Timeout(3 second)
   protected object NaoLogado extends Failure("Não logado!!!")
 
   protected def jsonApp = "application/json"
