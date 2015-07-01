928da2445090551ca02210e85685762897730721
Kleber Stender <kleber.stender@gmail.com>
2015-07-01 07:07:49 -0300

    Finished pattern desgin

--- app/br/com/wallet/types/loginTypes/Google.scala
+++ app/br/com/wallet/types/loginTypes/Google.scala
@@ -1,11 +1,15 @@
 package br.com.wallet.types.loginTypes
 
+import play.api.Configuration
+
 /**
  * Created by sirkleber on 29/06/15.
  */
-case class Google(clientId: Option[String], redirectUri: Option[String], secret: String) extends LoginTypes {
+case class Google(conf: Configuration, redirectUri: String) extends LoginTypes {
 
   def scope: String = "email%20profile"
 
   def authUrl: String = "https://accounts.google.com/o/oauth2/auth"
+
+  def loginType: String = "google"
 }
--- app/br/com/wallet/types/loginTypes/LoginTypes.scala
+++ app/br/com/wallet/types/loginTypes/LoginTypes.scala
@@ -1,16 +1,20 @@
 package br.com.wallet.types.loginTypes
 
+import play.api.Configuration
+
 /**
  * Created by sirkleber on 29/06/15.
  */
 trait LoginTypes {
-  def clientId: Option[String]
-  def secret: Option[String]
+  val conf: Configuration
+  def loginType: String
+  def clientId: Option[String] = conf.getString(s"${loginType}.client.id")
+  def secret: Option[String] = conf.getString(s"${loginType}.client.secret")
   def scope: String
-  def authUrl: Option[String]
+  def authUrl: String
   def redirectUri: String
 
-  def url: Option[String] = for {
+  def url = for {
     clId <- clientId
   } yield s"$authUrl?client_id=$clId&redirect_uri=$redirectUri&scope=$scope"
 }
