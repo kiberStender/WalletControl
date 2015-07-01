514902e3a2a29c7a68d06bc6731416161bcc679a
Kleber Stender <kleber.stender@gmail.com>
2015-07-01 09:32:36 -0300

    Removed state member

--- app/br/com/wallet/types/loginTypes/Google.scala
+++ app/br/com/wallet/types/loginTypes/Google.scala
@@ -5,11 +5,10 @@ import play.api.Configuration
 /**
  * Created by sirkleber on 29/06/15.
  */
-case class Google(conf: Configuration, redirectUri: String, state: String) extends LoginTypes {
+case class Google(conf: Configuration, redirectUri: String) extends LoginTypes {
+  def loginType: String = "google"
 
   def scope: String = "email%20profile"
 
   def authUrl: String = "https://accounts.google.com/o/oauth2/auth"
-
-  def loginType: String = "google"
 }
--- app/br/com/wallet/types/loginTypes/LoginTypes.scala
+++ app/br/com/wallet/types/loginTypes/LoginTypes.scala
@@ -6,16 +6,15 @@ import play.api.Configuration
  * Created by sirkleber on 29/06/15.
  */
 trait LoginTypes {
-  val conf: Configuration
-  def loginType: String
-  def clientId: Option[String] = conf.getString(s"${loginType}.client.id")
-  def secret: Option[String] = conf.getString(s"${loginType}.client.secret")
-  def state: String
-  def scope: String
-  def authUrl: String
-  def redirectUri: String
+  protected val conf: Configuration
+  protected def loginType: String
+  protected def clientId: Option[String] = conf.getString(s"${loginType}.client.id")
+  protected def secret: Option[String] = conf.getString(s"${loginType}.client.secret")
+  protected def scope: String
+  protected def authUrl: String
+  protected def redirectUri: String
 
   def url = for {
     clId <- clientId
-  } yield s"$authUrl?client_id=$clId&redirect_uri=$redirectUri&scope=$scope&state=$state"
+  } yield s"$authUrl?client_id=$clId&redirect_uri=$redirectUri&scope=$scope"
 }
