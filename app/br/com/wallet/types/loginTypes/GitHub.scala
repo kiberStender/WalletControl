80ec8a4a47bfe8a95c21952f499c7203698be39a
Kleber Stender <kleber.stender@gmail.com>
2015-07-06 19:13:50 -0300

    Changed the way to get redirectUri

--- app/br/com/wallet/types/loginTypes/GitHub.scala
+++ app/br/com/wallet/types/loginTypes/GitHub.scala
@@ -1,14 +1,17 @@
 package br.com.wallet.types.loginTypes
 
 import play.api.Configuration
+import play.api.mvc.Call
 
 /**
  * Created by sirkleber on 01/07/15.
  */
-case class GitHub(conf: Configuration, redirectUri: String) extends LoginTypes{
+case class GitHub(conf: Configuration, rUri:(String, Option[String], Option[String]) => Call) extends LoginTypes{
   override def loginType: String = "github"
 
   override def scope: String = "repo"
 
   override def authUrl: String = "https://github.com/login/oauth/authorize"
+
+  def redirectUri: String = rUri("google", None, None).toString
 }
--- app/br/com/wallet/types/loginTypes/Google.scala
+++ app/br/com/wallet/types/loginTypes/Google.scala
@@ -1,14 +1,17 @@
 package br.com.wallet.types.loginTypes
 
 import play.api.Configuration
+import play.api.mvc.Call
 
 /**
  * Created by sirkleber on 29/06/15.
  */
-case class Google(conf: Configuration, redirectUri: String) extends LoginTypes {
+case class Google(conf: Configuration, rUri:(String, Option[String], Option[String]) => Call) extends LoginTypes {
   def loginType: String = "google"
 
   def scope: String = "email%20profile&response_type=code"
 
   def authUrl: String = "https://accounts.google.com/o/oauth2/auth"
+
+  def redirectUri: String = rUri("google", None, None).toString
 }
