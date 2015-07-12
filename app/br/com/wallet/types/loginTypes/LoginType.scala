bf37cb981d534bbcf32310efbf9e3d8674c8a7f3
Kleber Stender <kleber.stender@gmail.com>
2015-07-12 08:33:24 -0300

    Changed again the way of creating a LoginTypes. Now it is just a case class followed by objetcts that extends it to create a constructor helper

--- app/br/com/wallet/types/loginTypes/LoginType.scala
+++ app/br/com/wallet/types/loginTypes/LoginType.scala
@@ -1,28 +1,51 @@
 package br.com.wallet.types.loginTypes
 
 import br.com.wallet.types.loginOption.LoginOption
+import play.api.Configuration
+import play.api.libs.json._
 import play.api.mvc.{Call, RequestHeader}
 
 /**
  * Created by sirkleber on 29/06/15.
  */
-trait LoginType {
-  protected def loginType: String
-  protected def clientId: Option[String]
-  protected def secret: Option[String]
-  protected def scope: String
-  protected def authUrl: String
-
-  def tokenUri: String
-
+sealed case class LoginType(
+  clientId: Option[String], secret: Option[String], provider: String, scope: String, authUrl: String, tokenUri: String
+) {
   def authData(
     tp: (String, (String, Option[String], Option[String]) => Call)
   )(implicit req: RequestHeader): Option[LoginOption] = tp match {
     case (state, redirectUri) => for {
       clId <- clientId
     } yield {
-        def rUri = redirectUri(loginType, None, None).absoluteURL()
-        LoginOption(loginType, s"$authUrl?client_id=$clId&redirect_uri=$rUri&scope=$scope&state=$state")
+        def rUri = redirectUri(provider, None, None).absoluteURL()
+        LoginOption(provider, s"$authUrl?client_id=$clId&redirect_uri=$rUri&scope=$scope&state=$state")
       }
+  }
+}
+
+object LoginType {
+  implicit val loginTypeWrite = Json.writes[LoginType]
+  implicit val loginTypeFmt = Json.format[LoginType]
+}
+
+object GithubType extends LoginType(
+  None, None, "github", "repo",
+  "https://github.com/login/oauth/authorize", "https://github.com/login/oauth/access_token"
+){
+  def apply(conf: Configuration): LoginType = {
+    lazy val clientId = conf getString s"$provider.client.id"
+    lazy val secret = conf getString s"$provider.client.secret"
+    LoginType(clientId, secret, provider, scope, authUrl, tokenUri)
+  }
+}
+
+object GoogleType extends LoginType(
+  None, None, "google", "email%20profile&response_type=code",
+  "https://accounts.google.com/o/oauth2/auth", "https://accounts.google.com/o/oauth2/token"
+){
+  def apply(conf: Configuration): LoginType = {
+    lazy val clientId = conf getString s"$provider.client.id"
+    lazy val secret = conf getString s"$provider.client.secret"
+    LoginType(clientId, secret, provider, scope, authUrl, tokenUri)
   }
 }
