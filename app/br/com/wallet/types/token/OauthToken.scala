db3d31291ff5970c600fcc8c5cfe359cc526669b
Kleber Stender <kleber.stender@gmail.com>
2015-07-21 10:09:41 -0300

    expiresIn is now optional

--- app/br/com/wallet/types/token/OauthToken.scala
+++ app/br/com/wallet/types/token/OauthToken.scala
@@ -5,7 +5,7 @@ import play.api.libs.json.{Format, Json}
 /**
  * Created by sirkleber on 21/07/15.
  */
-case class OauthToken(accessToken: String, tokenType: String, expiresIn: Int, idToken: String, refreshToken: String)
+case class OauthToken(accessToken: String, tokenType: String, expiresIn: Option[Int])
 
 object OauthToken {
   implicit def tokenFormatter: Format[OauthToken] = Json.format[OauthToken]
