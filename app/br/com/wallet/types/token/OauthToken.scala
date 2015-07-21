1aec2e3d248d4fcedb0e912ce79f1d165d07c650
Kleber Stender <kleber.stender@gmail.com>
2015-07-21 07:50:00 -0300

    Added forgotten implicit json formatter

--- app/br/com/wallet/types/token/OauthToken.scala
+++ app/br/com/wallet/types/token/OauthToken.scala
@@ -1,6 +1,12 @@
 package br.com.wallet.types.token
 
+import play.api.libs.json.{Format, Json}
+
 /**
  * Created by sirkleber on 21/07/15.
  */
 case class OauthToken(accessToken: String, tokenType: String, expiresIn: Int, idToken: String, refreshToken: String)
+
+object OauthToken {
+  implicit def tokenFormatter: Format[OauthToken] = Json.format[OauthToken]
+}
