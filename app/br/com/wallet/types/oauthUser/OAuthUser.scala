cea94cdd80b3a92bb776159485a1f45da637bc90
Kleber Stender <kleber.stender@gmail.com>
2015-07-21 10:06:16 -0300

    Removed implicit reader, token now is a OAuthToken instance and added logonTYpe for user data

--- app/br/com/wallet/types/oauthUser/OAuthUser.scala
+++ app/br/com/wallet/types/oauthUser/OAuthUser.scala
@@ -1,14 +1,15 @@
 package br.com.wallet.types.oauthUser
 
 import br.com.wallet.types.loginTypes.LoginType
+import br.com.wallet.types.logonType.LogonType
+import br.com.wallet.types.token.OauthToken
 import play.api.libs.json.{JsValue, Json}
 
 /**
  * Created by sirkleber on 12/07/15.
  */
-case class OAuthUser (user: String, state: String, code: String, token: JsValue, actualLogin: LoginType)
+case class OAuthUser (state: String, code: String, logonType: LogonType, token: OauthToken, actualLogin: LoginType)
 
 object OAuthUser {
-  implicit val writesUser = Json.writes[OAuthUser]
   implicit val formatter = Json.format[OAuthUser]
 }
