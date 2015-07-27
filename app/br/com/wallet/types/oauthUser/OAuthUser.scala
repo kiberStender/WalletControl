302021a494359f1c45537dadc0fbf587a308de29
Kleber Stender <kleber.stender@gmail.com>
2015-07-27 05:56:56 -0300

    Fixed LogonData import

--- app/br/com/wallet/types/oauthUser/OAuthUser.scala
+++ app/br/com/wallet/types/oauthUser/OAuthUser.scala
@@ -1,14 +1,14 @@
 package br.com.wallet.types.oauthUser
 
 import br.com.wallet.types.loginTypes.LoginType
-import br.com.wallet.types.logonType.LogonData$
+import br.com.wallet.types.logonType.LogonData
 import br.com.wallet.types.token.OauthToken
-import play.api.libs.json.{JsValue, Json}
+import play.api.libs.json.Json
 
 /**
  * Created by sirkleber on 12/07/15.
  */
-case class OAuthUser (state: String, code: String, logonType: LogonData, token: OauthToken, actualLogin: LoginType)
+case class OAuthUser (state: String, code: String, logonData: LogonData, token: OauthToken, actualLogin: LoginType)
 
 object OAuthUser {
   implicit val formatter = Json.format[OAuthUser]
