5df98c46cf0981a583399090e4f3192485b86773
Kleber Stender <kleber.stender@gmail.com>
2015-08-02 08:51:42 -0300

    Created methods queryRunnerSingle for getting only one or zero lines from a query, queryRunnerMany for getting 0 to n lines from a query and queryUpdate for inserting or updating a table

--- app/br/com/wallet/persistence/dao/Dao.scala
+++ app/br/com/wallet/persistence/dao/Dao.scala
@@ -1,6 +1,26 @@
 package br.com.wallet.persistence.dao
 
+import br.com.wallet.persistence.dto.Dto
+import play.api.db.DB
+import anorm.{NamedParameter, RowParser, SQL}
+import play.api.Play.current
+import java.sql.Connection
+
+import scala.concurrent.Future
+
 /**
  * Created by sirkleber on 4/6/15.
  */
-trait Dao
+trait Dao {
+  protected def queryRunnerSingle[T <: Dto](query: String)(parser: RowParser[T])(prmts: NamedParameter*): Future[T] = Future {
+    DB.withConnection {implicit conn => SQL(query).on(prmts: _*).as(parser.single)}
+  }
+
+  protected def queryRunnerMany[T <: Dto](query: String)(parser: RowParser[T])(prmts: NamedParameter*): Future[List[T]] = Future {
+    DB.withConnection {implicit conn => SQL(query).on(prmts: _*).as(parser *)}
+  }
+
+  protected def queryUpdate(query: String)(prmts: NamedParameter*): Future[Unit] = Future {
+    DB.withConnection{ implicit conn => SQL(query).on(prmts: _*).executeUpdate}
+  }
+}
