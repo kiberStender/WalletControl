246642d9666658fef41190e160fa1a9e0e7500db
Kleber Stender <kleber.stender@gmail.com>
2015-08-02 11:27:57 -0300

    queryRunnerSing and Many does not bound T type to Dto anymore

--- app/br/com/wallet/persistence/dao/Dao.scala
+++ app/br/com/wallet/persistence/dao/Dao.scala
@@ -1,10 +1,10 @@
 package br.com.wallet.persistence.dao
 
-import br.com.wallet.persistence.dto.Dto
 import play.api.db.DB
 import anorm.{NamedParameter, RowParser, SQL}
 import play.api.Play.current
 import java.sql.Connection
+import scala.concurrent.ExecutionContext.Implicits.global
 
 import scala.concurrent.Future
 
@@ -12,11 +12,11 @@ import scala.concurrent.Future
  * Created by sirkleber on 4/6/15.
  */
 trait Dao {
-  protected def queryRunnerSingle[T <: Dto](query: String)(parser: RowParser[T])(prmts: NamedParameter*): Future[T] = Future {
+  protected def queryRunnerSingle[T](query: String)(parser: RowParser[T])(prmts: NamedParameter*): Future[T] = Future {
     DB.withConnection {implicit conn => SQL(query).on(prmts: _*).as(parser.single)}
   }
 
-  protected def queryRunnerMany[T <: Dto](query: String)(parser: RowParser[T])(prmts: NamedParameter*): Future[List[T]] = Future {
+  protected def queryRunnerMany[T](query: String)(parser: RowParser[T])(prmts: NamedParameter*): Future[List[T]] = Future {
     DB.withConnection {implicit conn => SQL(query).on(prmts: _*).as(parser *)}
   }
 
