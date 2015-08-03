ca00a3ab3878fc00829236f1fe5fb66bca2c11f9
Kleber Stender <kleber.stender@gmail.com>
2015-08-03 07:34:27 -0300

    Removed useless imports and objects

--- app/br/com/wallet/api/controller/ActionController.scala
+++ app/br/com/wallet/api/controller/ActionController.scala
@@ -1,20 +1,14 @@
 package br.com.wallet.api.controller
 
 import akka.util.Timeout
-import br.com.wallet.api.models.result.{Success, Failure}
-import br.com.wallet.types.AccUser
 import scala.concurrent.duration._
 import play.api.mvc._
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
-import play.api.libs.json.{JsValue, JsObject, Json}
 
 /**
  * Created by sirkleber on 4/7/15.
  */
 trait ActionController extends Controller {
   protected implicit lazy val timeout = Timeout(3 second)
-  protected object NaoLogado extends Failure("Não logado!!!")
 
   protected def jsonApp = "application/json"
 
