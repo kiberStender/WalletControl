4619e87bbd9bf2384953d8e87158e7e37a4f8cfa
Kleber Stender <kleber.stender@gmail.com>
2015-08-12 07:09:03 -0300

    Added json format logic

--- app/br/com/wallet/types/transaction/request/SpreadsheetRequest.scala
+++ app/br/com/wallet/types/transaction/request/SpreadsheetRequest.scala
@@ -1,6 +1,12 @@
 package br.com.wallet.types.transaction.request
 
+import play.api.libs.json.{Json, Format}
+
 /**
  * Created by sirkleber on 12/08/15.
  */
-class SpreadsheetRequest extends AbstractRequest
+case class SpreadsheetRequest() extends AbstractRequest
+
+object SpreadsheetRequest {
+  implicit def formatter: Format[SpreadsheetRequest] = Json.format[SpreadsheetRequest]
+}
