ef09324118426efc0be176ea4505f12749d35da4
Kleber Stender <kleber.stender@gmail.com>
2015-08-12 07:32:56 -0300

    Added attribute method

--- app/br/com/wallet/types/transaction/request/SpreadsheetRequest.scala
+++ app/br/com/wallet/types/transaction/request/SpreadsheetRequest.scala
@@ -5,7 +5,7 @@ import play.api.libs.json.{Json, Format}
 /**
  * Created by sirkleber on 12/08/15.
  */
-case class SpreadsheetRequest() extends AbstractRequest
+case class SpreadsheetRequest(method: String) extends AbstractRequest
 
 object SpreadsheetRequest {
   implicit def formatter: Format[SpreadsheetRequest] = Json.format[SpreadsheetRequest]
