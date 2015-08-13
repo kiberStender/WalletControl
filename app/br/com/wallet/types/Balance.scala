446f951ba2628f91fe08b2ed75ff0cd328e1e436
Kleber Stender <kleber.stender@gmail.com>
2015-08-13 08:01:41 -0300

    Removed old data table mappers

--- app/br/com/wallet/types/Balance.scala
+++
@@ -1,8 +0,0 @@
-package br.com.wallet.types
-
-import com.github.nscala_time.time.Imports._
-
-/**
- * Created by sirkleber on 4/6/15.
- */
-case class Balance(balanceid: String, typeid : String, calcbalance: Double, realbalance: Double, balancedate: DateTime)
