3f68abd4c06fd295ade63270b300d1250ab4e820
Kleber Stender <kleber.stender@gmail.com>
2015-08-22 17:51:40 -0300

    Added value in item class

--- app/br/com/wallet/persistence/dto/Item.scala
+++ app/br/com/wallet/persistence/dto/Item.scala
@@ -7,8 +7,11 @@ import play.api.libs.json.{Json, Format}
 /**
  * Created by sirkleber on 13/08/15.
  */
-case class Item(itemId: String, description: String, purchaseDate: Date, trtType: String) extends Dto {
-  override def toString = s"{itemId: $itemId, description: $description, purchaseDate: $purchaseDate, trtType: $trtType}"
+case class Item(
+  itemId: String, description: String, value: Double, purchaseDate: Date, trtType: String
+) extends Dto {
+  override def toString =
+    s"{itemId: $itemId, description: $description, value: $value, purchaseDate: $purchaseDate, trtType: $trtType}"
 }
 
 object Item {
