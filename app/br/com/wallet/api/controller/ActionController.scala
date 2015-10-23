ed76491137be3858b570467b30ddd0d5c8b0fa84
Kleber Stender <kleber.stender@gmail.com>
2015-10-23 11:14:41 -0200

    Created validateSession protected method

--- app/br/com/wallet/api/controller/ActionController.scala
+++ app/br/com/wallet/api/controller/ActionController.scala
@@ -1,8 +1,11 @@
 package br.com.wallet.api.controller
 
 import akka.util.Timeout
+import br.com.wallet.api.models.result.Failure
+import scala.concurrent.Future
 import scala.concurrent.duration._
 import play.api.mvc._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 /**
  * Created by sirkleber on 4/7/15.
@@ -14,4 +17,13 @@ trait ActionController extends Controller {
 
   protected def oauthUserSession = "oauth_user"
   protected def oauthStateSesion = "oauth_state"
+
+  protected def validateSession: (String, String) => (Request[AnyContent] => Future[Result]) => Action[AnyContent] = {
+    case (state, userid) => f => Action.async {req =>
+      req.session.get(oauthUserSession) match {
+        case Some(userJson) => f(req)
+        case None => Future{Ok(Failure("Sessão expirou") toJson) as jsonApp}
+      }
+    }
+  }
 }
