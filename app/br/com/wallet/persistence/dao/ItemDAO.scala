f0f224ac5543f448e72c6d0c8cb2e1d98cd728d4
Kleber Stender <kleber.stender@gmail.com>
2015-10-26 14:45:02 -0200

    Added insertItem function

--- app/br/com/wallet/persistence/dao/ItemDAO.scala
+++ app/br/com/wallet/persistence/dao/ItemDAO.scala
@@ -3,10 +3,23 @@ package br.com.wallet.persistence.dao
 import anorm.SqlParser._
 import br.com.wallet.persistence.dto.Item
 
+import scala.concurrent.Future
+
 /**
  * Created by sirkleber on 13/08/15.
  */
 object ItemDAO extends Dao {
+
+  def insertItem: (String, String, Item) => Future[Unit] = {
+    case (acctypeid, accuserid, Item(id, desc, value, date, typeid)) => queryUpdate(
+      """Insert into item(itemid, transactiontypeid, acctypeid, accuserid, description, itemvalue, purchasedate) values(
+          {id}, {trtypeid}, {acctypeid}, {accuserid}, {desc}, {vl}, {purchasedt}
+         )"""
+    )(
+      'id -> id, 'trtypeid -> typeid, 'acctypeid -> acctypeid,'accuserid -> accuserid, 'desc -> desc,
+      'vl -> value, 'purchasedt -> date
+    )
+  }
 
   def getMonthItemsByAccountType(acctypeId: String, userid: String, month: Int) = queryRunnerManyS(
     """Select i.itemid, t.transactiontypename, i.description, i.itemvalue, i.purchasedate
