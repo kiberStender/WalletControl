1a2ae9e84a3f6c40eaa5641163a3e50f0d5db407
Kleber Stender <kleber.stender@gmail.com>
2015-10-27 19:48:29 -0200

    Improved to assist state validation

--- app/br/com/wallet/api/controller/ActionController.scala
+++ app/br/com/wallet/api/controller/ActionController.scala
@@ -1,5 +1,7 @@
 package br.com.wallet.api.controller
 
+import br.com.wallet.types.oauthUser.OAuthUser
+import play.api.libs.json.Json
 import akka.util.Timeout
 import br.com.wallet.api.models.result.Failure
 import scala.concurrent.Future
@@ -18,10 +20,16 @@ trait ActionController extends Controller {
   protected def oauthUserSession = "oauth_user"
   protected def oauthStateSesion = "oauth_state"
 
-  protected def validateSession: (String, String) => (Request[AnyContent] => Future[Result]) => Action[AnyContent] = {
+  protected def validateSession: (String, String) => ((OAuthUser, Request[AnyContent]) => Future[Result]) => Action[AnyContent] = {
     case (state, userid) => f => Action.async {req =>
       req.session.get(oauthUserSession) match {
-        case Some(userJson) => f(req)
+        case Some(userJson) => (for {
+          user <- Json.parse(userJson).validate[OAuthUser]
+        } yield if(user.state == state){
+            f(user, req)
+          } else {
+            Future(Ok(Failure("Sessão expirou") toJson) as jsonApp)
+          }).getOrElse(Future(Ok(Failure("Erro ao processar dados do usuário") toJson) as jsonApp))
         case None => Future{Ok(Failure("Sessão expirou") toJson) as jsonApp}
       }
     }
